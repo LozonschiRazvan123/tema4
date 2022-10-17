f540de3bfbdeed41aa636dc928c4cb63f10287e4
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2022-10-17 09:11:04 +0300

    Add project files.

---
+++ tema4/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace tema4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(350, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxA.Location = new System.Drawing.Point(18, 108);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 26);
+            this.textBoxA.TabIndex = 3;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxC.Location = new System.Drawing.Point(313, 108);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 26);
+            this.textBoxC.TabIndex = 4;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxB.Location = new System.Drawing.Point(164, 108);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 26);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCheck.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonCheck.Location = new System.Drawing.Point(189, 168);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 36);
+            this.buttonCheck.TabIndex = 6;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Button buttonCheck;
+    }
+}
+
---
+++ tema4/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tema4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            double a, b, c, d, x1, x2;
+            try
+            {
+                a = Double.Parse(textBoxA.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("'A' should be a double number");
+                return;
+            }
+            try
+            {
+                b = Double.Parse(textBoxB.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("'B' should be a double number");
+                return;
+            }
+            try
+            {
+                c = Double.Parse(textBoxC.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("'C' should be a double number");
+                return;
+            }
+            d = b * b - 4 * a * c;
+            if (d >= 0)
+                if (d == 0)
+
+                {
+                    x1 = x2 = -b / (2 * a);
+                    MessageBox.Show("Solution are " + x1 +" and "+ + x2);
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    MessageBox.Show("Solutions are" + x1 + " and " + +x2);
+                }
+            else
+                MessageBox.Show("No solution");
+        }
+    }
+}
